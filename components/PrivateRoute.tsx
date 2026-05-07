f7cd0b8d94df1bd1e1b507207b6a8f6a7acb18f2
DaniloPalma <danilodpalma@gmailc.com>
2026-05-07 15:17:00 -0300

    Ajustes visuais

---
+++ components/PrivateRoute.tsx
@@ -0,0 +1,31 @@
+// components/ProtectedRoute.tsx
+import { useAuth } from "../context/AuthContext";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function ProtectedRoute({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { user, isLoading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.push("/login");
+    }
+  }, [user, isLoading, router]);
+
+  if (isLoading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        Carregando...
+      </div>
+    );
+  }
+
+  if (!user) return null;
+
+  return <>{children}</>;
+}
