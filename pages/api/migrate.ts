1c6e49c9a0142ff9708d4b228f846ee71af367ae
DaniloPalma <danilodpalma@gmailc.com>
2026-09-08 11:23:52 -0300

    novomigrate

--- pages/api/migrate.ts
+++ pages/api/migrate.ts
@@ -8,10 +8,14 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     return res.status(401).json({ error: "Não autorizado" });
   }
 
-  exec("npx prisma db push", (error, stdout, stderr) => {
-    if (error) {
-      return res.status(500).json({ error: error.message, stderr });
-    }
-    return res.status(200).json({ success: true, output: stdout });
-  });
+  exec(
+    "node ./node_modules/prisma/build/index.js db push --accept-data-loss",
+    { env: { ...process.env, HOME: "/tmp" } },
+    (error, stdout, stderr) => {
+      if (error) {
+        return res.status(500).json({ error: error.message, stderr });
+      }
+      return res.status(200).json({ success: true, output: stdout });
+    },
+  );
 }
