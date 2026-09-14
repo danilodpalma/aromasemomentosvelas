d78ab2e4391ec18f7d0d23f4978f7e6099c51a3b
DaniloPalma <danilodpalma@gmailc.com>
2026-09-14 15:08:35 -0300

    separação das linhas

--- components/modelos/ModeloForm.tsx
+++ components/modelos/ModeloForm.tsx
@@ -1448,6 +1448,15 @@ export default function ModeloForm({
               }}
             />
           </label>
+          <p
+            style={{
+              margin: "0 0 12px",
+              color: COLORS.primaryDarkAlt,
+              fontSize: 13,
+            }}
+          >
+            Insumos Usados Somente para Sabonetes.
+          </p>
           <label style={{ display: "block" }}>
             Extrato
             <select
@@ -1688,6 +1697,15 @@ export default function ModeloForm({
               }}
             />
           </label>
+           <p
+            style={{
+              margin: "0 0 12px",
+              color: COLORS.primaryDarkAlt,
+              fontSize: 13,
+            }}
+          >
+            Custos Fixos e Margem%.
+          </p>
           <label style={{ display: "block" }}>
             Embalagem (R$)
             <input
